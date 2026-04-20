bd89467137b15212afca796280fab242fa91423a
Yash Kushwaha <kushwahayashh@gmail.com>
2026-04-20 17:13:52 +0530

    Remove header from app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { getTodos } from "./actions";
 import { TodoApp } from "./components/todo-app";
-import { IconCircleCheck } from "@tabler/icons-react";
 
 export const dynamic = "force-dynamic";
 
@@ -9,14 +8,6 @@ export default async function Home() {
 
   return (
     <div className="flex min-h-dvh flex-col bg-white">
-      <div className="sticky top-0 bg-neutral-200 px-4 py-3">
-        <div className="mx-auto w-full max-w-md">
-          <h1 className="text-lg font-bold text-black flex items-center gap-2">
-            <IconCircleCheck size={20} stroke={2} />
-            AlexTodo
-          </h1>
-        </div>
-      </div>
       <div className="flex-1 px-4 pt-6 pb-24">
         <div className="mx-auto w-full max-w-md">
           <TodoApp todos={todos} />
